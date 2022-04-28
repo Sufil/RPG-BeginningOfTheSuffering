77745f356452d61c671936a2ba57d83a2778cb36
Admin <Admin@DESKTOP-6D5MPJ9>
2022-04-28 20:48:33 +0200

    Projektdateien hinzufügen.

---
+++ Privat_RPG/Class/Enemys/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Privat_RPG
+{
+    class Enemy
+    {
+        int hp;
+        int lvl;
+        int probabilityOfAppearing;
+        string name;
+
+        Attack[] attacks;
+    }
+}
---
+++ Privat_RPG/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Privat_RPG
+{
+    class Program
+    {
+        static public bool yourMama = true;
+        static public bool fat = true;
+        static void Main(string[] args)
+        {
+            while (yourMama == fat)
+            {
+
+            }
+
+        }
+        
+        
+    }
+}
